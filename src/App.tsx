bafab673b9b9b100b60cc37c566be548a0de969b
Constantine Zubarev <kv.zubarev@gmail.com>
2022-08-12 15:22:29 +0600

    update: config
    * alias
    * dev sourcemaps
    * specific index.html folder
    * svg as react component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import {ReactComponent as ReactLogo} from '@@/assets/react.svg'
+import '@@/App.css'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,7 +12,7 @@ function App() {
           <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
         <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <ReactLogo className="logo react" width="144" height="144"/>
         </a>
       </div>
       <h1>Vite + React</h1>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,28 @@
 import { defineConfig } from 'vite'
+import { fileURLToPath, URL } from 'node:url'
+import { createHtmlPlugin } from 'vite-plugin-html'
+import svgr from 'vite-plugin-svgr'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [
+    react(),
+    svgr(),
+
+    createHtmlPlugin({
+      entry: '/src/main.tsx',
+      template: 'public/index.html',
+    })
+  ],
+
+  resolve: {
+    alias: {
+      '@@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+
+  css: {
+    devSourcemap: true
+  }
 })
